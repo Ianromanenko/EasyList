f74255b85fe46f0412630e20d4bd21974e85abf8
yan <ianromanenko@gmail.com>
2017-12-06 20:34:15 -0500

    logic of shopping item count created, default state and values added

--- EasyList/ViewController.swift
+++ EasyList/ViewController.swift
@@ -2,7 +2,7 @@
 // MAPD 714 - Mid Term Exam
 //
 // App name: Easy List
-// Version: 0.01 - Basic prep
+// Version: 0.02 - Logic to handle itme change added, default state of the app
 // Description: Shopping list application
 // Collaborators: 300964200 - Viktor Bilyk
 //                300965775 - Timofei Sopin
@@ -36,21 +36,136 @@ class ViewController: UIViewController {
 
     
     //ACTION METHODS
+    
+    func shoppingItemCountChange (itemNumber: Int, operation: Bool) {
+        // changing quantity of shopping item
+        
+        switch itemNumber {
+        case 1: //item 1
+            if operation {
+                //PLUS SELECTED
+                shoppingListItem1Value = shoppingListItem1Value + 1
+            }
+            else {
+                //MINUS SELECTED
+                if (shoppingListItem1Value - 1) >= 0 {
+                    shoppingListItem1Value = shoppingListItem1Value - 1
+                }
+                
+            }
+            shoppingListItem1CountLabel.text = String(shoppingListItem1Value)
+        case 2: //item 2
+            if operation {
+                //PLUS SELECTED
+                shoppingListItem2Value = shoppingListItem2Value + 1
+            }
+            else {
+                //MINUS SELECTED
+                if (shoppingListItem2Value - 1) >= 0 {
+                    shoppingListItem2Value = shoppingListItem2Value - 1
+                }
+                
+            }
+            shoppingListItem2CountLabel.text = String(shoppingListItem2Value)
+        case 3: //item 3
+            if operation {
+                //PLUS SELECTED
+                shoppingListItem3Value = shoppingListItem3Value + 1
+            }
+            else {
+                //MINUS SELECTED
+                if (shoppingListItem3Value - 1) >= 0 {
+                    shoppingListItem3Value = shoppingListItem3Value - 1
+                }
+                
+            }
+            shoppingListItem3CountLabel.text = String(shoppingListItem3Value)
+        case 4: //item 4
+            if operation {
+                //PLUS SELECTED
+                shoppingListItem4Value = shoppingListItem4Value + 1
+            }
+            else {
+                //MINUS SELECTED
+                if (shoppingListItem4Value - 1) >= 0 {
+                    shoppingListItem4Value = shoppingListItem4Value - 1
+                }
+                
+            }
+            shoppingListItem4CountLabel.text = String(shoppingListItem4Value)
+        case 5: //item 5
+            if operation {
+                //PLUS SELECTED
+                shoppingListItem5Value = shoppingListItem5Value + 1
+            }
+            else {
+                //MINUS SELECTED
+                if (shoppingListItem5Value - 1) >= 0 {
+                    shoppingListItem5Value = shoppingListItem5Value - 1
+                }
+                
+            }
+            shoppingListItem5CountLabel.text = String(shoppingListItem5Value)
+        default: break
+        }
+       
+    }
+    
     @IBAction func shoppingListFieldDoneAction(_ sender: UITextField) {
         //
     }
     
     @IBAction func onStepperControlPress(_ sender: UIStepper)  {
         //Action to handle STEPPER press to change list count
+        
     }
     
     @IBAction func onCancelButtonClick(_ sender: UIButton) {
         //Action to reset app to default state
+        shoppingListItem1Value = 0
+        shoppingListItem2Value = 0
+        shoppingListItem3Value = 0
+        shoppingListItem4Value = 0
+        shoppingListItem5Value = 0
+        
+        shoppingListItem1CountLabel.text = "0"
+        shoppingListItem2CountLabel.text = "0"
+        shoppingListItem3CountLabel.text = "0"
+        shoppingListItem4CountLabel.text = "0"
+        shoppingListItem5CountLabel.text = "0"
+        
+        shoppingListItem1.text = " "
+        shoppingListItem2.text = " "
+        shoppingListItem3.text = " "
+        shoppingListItem4.text = " "
+        shoppingListItem5.text = " "
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //default values
+        shoppingListItem1.text = " "
+        shoppingListItem2.text = " "
+        shoppingListItem3.text = " "
+        shoppingListItem4.text = " "
+        shoppingListItem5.text = " "
+        
+        shoppingListItem1CountLabel.text = "0"
+        shoppingListItem2CountLabel.text = "0"
+        shoppingListItem3CountLabel.text = "0"
+        shoppingListItem4CountLabel.text = "0"
+        shoppingListItem5CountLabel.text = "0"
+        
+        shoppingListItem1Value = 0
+        shoppingListItem2Value = 0
+        shoppingListItem3Value = 0
+        shoppingListItem4Value = 0
+        shoppingListItem5Value = 0
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
